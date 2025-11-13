00e76a0c8ea68a880612194d8e44a4f21332e4e6
erAbhishekJaiswal <erabhishek72@gmail.com>
2025-11-13 18:08:06 +0530

    create the api for User Details for the admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,6 +24,19 @@ exports.getUserById = async (req, res) => {
     }
 };
 
+// get user detail for Admin
+exports.getUserDetail = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).select("-password");
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
 // create user
 exports.createUser = async (req, res) => {
     try {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers,getUserById,createUser,updateUser,deleteUser,updateUserStatus } = require("../controllers/userController");
+const { getAllUsers,getUserById,getUserDetail, createUser,updateUser,deleteUser,updateUserStatus } = require("../controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/", getAllUsers);
 router.get("/profile", protect, getUserById);
 router.post("/", createUser);
+router.get("/:id", getUserDetail);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 router.put("/:id/status", updateUserStatus);
